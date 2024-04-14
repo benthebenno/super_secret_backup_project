f97ce676266ed9c10ea84b47a01c00b64b9b6499
benthebenno <ben.rappoport@gmail.com>
2024-04-14 01:30:26 -0400

    final push

--- README.txt
+++ README.txt
@@ -1,9 +1,28 @@
 Welcome to Save Obie!!!!
 
+Created by Benjamin Rappoport and Jesse Meislich
+
 Citations:
 
-    Image Design for the Squirell goes to 
+    Image Design for the Squirell goes to:
         https://www.reddit.com/r/PixelArt/comments/ppdndk/i_drew_a_small_squirrel/
-        
+
+    Bomb Design goes to:
+        https://en.ac-illust.com/clip-art/23562287/bomb-pixel-art#google_vignette
+
+    Explosion Design goes to:
+        https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dpixel%2Bexplosion&psig=AOvVaw3Z-yARgYstHeT62ZiFj7xI&ust=1713156850550000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCMwuT0wIUDFQAAAAAdAAAAABAE
+
+
 How to Play:
+
+    The goal of this game is to make sure the increasing number Squirrels are safe and do not stumble into any bombs that were carelessly left around. 
+    Every turn you have the choice to select a squirrel to move while the rest will move any way that pleases them. Enter p to safely exit.
+    Good luck
+
+    P.S Squirrels are friendly and will share the same square.
+
+    P.P.S Obie is Squirrel number one, because of course he is.
+
+Current High Score: 15
 
--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from pygame.locals import *
 import time
 
 
-
-
 class Bomb:
     def  __init__(self):
-        self.x=random.randint(0,10)
-        self.y=random.randint(0,10)
+        self.x=random.randint(0,9)
+        self.y=random.randint(0,9)
 
 
     
@@ -20,36 +18,6 @@ class Squirrel():
         self.x=x
         self.y=y
         self.alive = True
-        
-    # def nextrandommove(self):
-    #     rando= random.randint(0,4)
-    #     if rando==0:
-    #         self.x-=1
-    #     elif rando==1:
-    #         self.x+=1
-    #     elif rando==2:
-    #         self.y+=1
-    #     elif rando==3:
-    #         self.y+=1
-
-
-    # def nextcontrolmove(self, int):
-    #     if int=="left":
-    #         if self.x-1<0:
-    #             self.x=10
-    #         self.x-=1
-    #     elif int=="right":
-    #         if self.x+1>9:
-    #             self.x=-1
-    #         self.x+=1
-    #     elif int=="up":
-    #         if self.x+1>9:
-    #             self.x=0
-    #         self.y+=1
-    #     elif int=="down":
-    #         if self.x+1>9:
-    #             self.x=-1
-    #         self.y+=1
 
     def boomquestionmark(self, bomblist_x,bomblist_y):
         for i in range(3):
@@ -84,44 +52,58 @@ def main():
     active = True
 
     screen = pygame.display.set_mode((1000,1000))
-    # clock = pygame.time.Clock()
     background = pygame.image.load('background.png').convert()
     screen.blit(background, (0, 0))
     firstsquirrel = Squirrel(1,5,5)
     numberofsquirrels = 1
 
 
-    image1_not_scaled = pygame.image.load("squirrel_template_1.png")
+    image1_not_scaled = pygame.image.load("whitesquirrel.png")
     image1 = pygame.transform.scale(image1_not_scaled, (90, 90))  
 
-    # list.append(firstsquirrel)
     screen.blit(image1, (coor(firstsquirrel.x),coor(firstsquirrel.y)))
     alivesquirrels = []
     alivesquirrels.append(firstsquirrel)
-    # [firstsquirrel]
 
 
     bomb_not_scaled = pygame.image.load("bomb.png")
     bomb_image = pygame.transform.scale(bomb_not_scaled, (90, 90))  
-
     bomb1=Bomb()
+    bomb2=Bomb()
+    bomb3=Bomb()
+    
+    bomb1.x=5
+    bomb1.y=5
+    bomb2.x=5
+    bomb2.y=5
+    bomb3.x=5
+    bomb3.y=5
+
+    # This is Jesses pride and joy, please appriciate 
+    while( ( (bomb1.x==5 and bomb1.y==5))) or  (bomb2.x==5 and bomb2.y==5) or ( (bomb3.x==5 and bomb3.y==5)) or ((bomb3.x==bomb1.x and bomb3.y==bomb1.y) or (bomb2.x==bomb3.x and bomb2.y==bomb3.y)) or(bomb2.x==bomb1.x and bomb2.y==bomb1.y):
+        bomb1=Bomb()
+       
+
+
+        bomb2=Bomb()
+        
+
+        
+        bomb3=Bomb()
+        
     screen.blit(bomb_image, (coor(bomb1.x),coor(bomb1.y)))
     bomblist_x.append(bomb1.x)
     bomblist_y.append(bomb1.y)
-
-
-    bomb2=Bomb()
     screen.blit(bomb_image, (coor(bomb2.x),coor(bomb2.y)))
     bomblist_x.append(bomb2.x)
     bomblist_y.append(bomb2.y)
-
-    
-    bomb3=Bomb()
     screen.blit(bomb_image, (coor(bomb3.x),coor(bomb3.y)))
     bomblist_x.append(bomb3.x)
     bomblist_y.append(bomb3.y)
-
-
+    print("bomblist x")
+    print(bomblist_x)
+    print("bomblist y")
+    print(bomblist_y)
 
     pygame.display.set_caption('Save Yobie')
    
@@ -132,181 +114,143 @@ def main():
         pygame.draw.line(screen,line_color, (0, (99.9*i)), (1000, (99.9*i)))
 
     pygame.display.update()
+
     turns = 0
-    con=0
+    con = 0
     key = "none"
 
 
     while active:
+
         
-        if turns%9==0 and numberofsquirrels<8:
-            i=numberofsquirrels+1
-            nextaddedsquirrel = Squirrel(i,random.randint(0,10),random.randint(0,10))
+        if  numberofsquirrels<8:
+            i = numberofsquirrels+1
+            nextaddedsquirrel = Squirrel(i,random.randint(0,9),random.randint(0,9))
             alivesquirrels.append(nextaddedsquirrel)
             numberofsquirrels += 1
 
-        for event in pygame.event.get():
-
-            if event.type == pygame.KEYDOWN:
-                print( "A key is pressed down" )
-
-                if event.key == pygame.K_1:
-                    print("hellodedfe")
-                    con = 1
-                if event.key == pygame.K_2:
-                    con = 2
-                if event.key == pygame.K_3:
-                    con = 3
-                if event.key == pygame.K_4:
-                    con = 4
-                if event.key == pygame.K_5:
-                    con = 5
-                if event.key == pygame.K_6:
-                    con = 6
-                if event.key == pygame.K_7:
-                    con = 7
-                if event.key == pygame.K_8:
-                    con = 8
-                if event.key == pygame.K_9:
-                    con =9
-
-                
-
-                if event.key == pygame.K_UP:
-                   print("uppies")
-                   key = "up"
-                if event.key == pygame.K_LEFT:
-                    key = "left"
-                if event.key == pygame.K_RIGHT:
-                    key = "right"
-                if event.key == pygame.K_DOWN:
-                    key = "down"
-                if event.key == pygame.K_ESCAPE:
-                    active = False
-                    pygame.quit()
-                    quit()
-         
-
-            
-            count=0
-            
-            # for bomb in range(3):
-            #     for squirrel in alivesquirrels:
-            #         if bomblist_x[bomb]==squirrel.x:
-            #             if bomblist_y[bomb]==squirrel.y:
-            #                 alivesquirrels.remove(squirrel)
-
-            # list.clear()
-
-            # for i in alivesquirrels:
-            #     list.append(i.x)
-            #     list.append(i.y)
-
-            for squirrel in alivesquirrels:
-
-                
-                replacement = pygame.image.load('background.png')
-                rep = pygame.transform.scale(replacement, (90, 90))
-                screen.blit(rep, (coor(squirrel.x),coor(squirrel.y)))
-                pygame.display.update()
-
-                if squirrel.boomquestionmark(bomblist_x,bomblist_y):
-
-                    explosion = pygame.image.load("explosion.png")
-                    # explosion_image = pygame.transform.scale(explosion, (90, 90)) 
-                    screen.blit(explosion, (0,0))
-                    pygame.display.update()
-                    print("boo hooo")
-                    time.sleep(4)
-                    pygame.quit()
-                    quit()
-
-
-                #This replaces the Squirrel with a green block
-                
-
-                # print(alivesquirrels)
-                # print(squirrel.num)
-                squirrel_file = str(squirrelImage(squirrel.num))
-
-
-                print(squirrel.num)
-                print(con)
-                print("1 num, 2 con")
-                if squirrel.num == con:
-                    if key == "up":
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.y+1>9:
-                            squirrel.y=0
-                        screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y+1)))
-                        squirrel.y =squirrel.y + 1 
-                    if key == "down":
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.y-1<0:
-                            squirrel.y=9
-                        screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y-1)))  
-                        squirrel.y = squirrel.y - 1
-                    if key == "left":
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.x-1<0:
-                            squirrel.x=9
-                        screen.blit(temp_image, (coor(squirrel.x-1),coor(squirrel.y))) 
-                        squirrel.x = squirrel.x - 1 
-                    if key == "right":
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.image.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.x+1>9:
-                            squirrel.x=0
-                        screen.blit(temp_image, (coor(squirrel.x+1),coor(squirrel.y)))    
-                        squirrel.x = squirrel.x + 1
-                    pygame.display.update()
-
-                else:
-                    rand_num = random.randint(0,4)
-                    if rand_num == 0:
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.y+1>9:
-                            squirrel.y=0
-                        screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y+1)))
-                        squirrel.y =squirrel.y+1
-
-                    if rand_num == 1:
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.y-1<0:
-                            squirrel.y=9
-                        screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y-1)))  
-                        squirrel.y = squirrel.y -1
-                        
-                    if rand_num == 2:
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.x-1<0:
-                            squirrel.x=9
-                        screen.blit(temp_image, (coor(squirrel.x-1),coor(squirrel.y)))
-                        squirrel.x = squirrel.x -1
-
-                    if rand_num == 3:
-                        temp_image_not_scaled = pygame.image.load(squirrel_file)
-                        temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
-                        if squirrel.x+1>9:
-                            squirrel.x=0
-                        screen.blit(temp_image, (coor(squirrel.x+1),coor(squirrel.y))) 
-                        squirrel.x = squirrel.x +1
-                
-                
+        con = str(input('Squirrel Choice:'))
+        key = str(input("Movement:"))
+        
+        if key == "p":
+            pygame.quit()
+            quit()
+    
+        #This for loop will iterate through all the current squirrels and move them
+        for squirrel in alivesquirrels:
+            replacement = pygame.image.load('background.png')
+            rep = pygame.transform.scale(replacement, (90, 90))
+            screen.blit(rep, (coor(squirrel.x),coor(squirrel.y)))
             pygame.display.update()
-            time.sleep(1)
-            time_count = time_count + 1
-            print("Congrats you have protected your squirrels for " + str(time_count) + " seconds")
+            print("Squirrel x:"+str(squirrel.x))
+            print("Squirrel y:"+str(squirrel.y))
             
             
+            #This if will check if a Squirrel is on a bomb
+            if squirrel.boomquestionmark(bomblist_x,bomblist_y):
 
+                explosion = pygame.image.load("explosion.png")
+                screen.blit(explosion, (0,0))
+                pygame.display.update()
+                print("boo hooo")
+                time.sleep(4)
+                pygame.quit()
+                quit()
 
+            squirrel_file = str(squirrelImage(squirrel.num))
+
+        # If this is the Squirrel you selected it will allow you to move it, depending on the direction specified
+            if str(squirrel.num) == str(con):
+ 
+                if key == "w":
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    squirrel.y =squirrel.y - 1 
+                    if squirrel.y<0:
+                        squirrel.y=9
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y)))
+                    
+                    
+                    print("Squirrel y changed:"+str(squirrel.y))
+
+                if key == "s":
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.y = squirrel.y + 1
+                    if squirrel.y>9:
+                        squirrel.y=0
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y)))  
+                    
+                if key == "a":
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.x = squirrel.x - 1 
+                    if squirrel.x<0:
+                        squirrel.x=9
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y))) 
+                if key == "d":
+
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.x = squirrel.x + 1
+                    if squirrel.x>9:
+                        squirrel.x=0
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y))) 
+              
+                    
+            #This will move the Squirrel in a random direction
+            else:
+            
+                rand_num = random.randint(0,3)
+
+                #up
+                if rand_num == 0:
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.y =squirrel.y-1
+                    if squirrel.y>9:
+                        squirrel.y=0
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y)))
+                    
+                    #down
+                if rand_num == 1:
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.y = squirrel.y + 1
+                    if squirrel.y<0:
+                        squirrel.y=9
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y)))  
+                    
+                    #left
+                if rand_num == 2:
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.x = squirrel.x -1
+                    if squirrel.x<0:
+                        squirrel.x=9
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y)))
+                    
+                    #right
+                if rand_num == 3:
+                    temp_image_not_scaled = pygame.image.load(squirrel_file)
+                    temp_image = pygame.transform.scale(temp_image_not_scaled, (90, 90))
+                    
+                    squirrel.x = squirrel.x +1
+                    if squirrel.x>9:
+                        squirrel.x=0
+                    screen.blit(temp_image, (coor(squirrel.x),coor(squirrel.y))) 
+            print("squirrel number "+str(squirrel.num)+" squirrel square ("+str(squirrel.x)+','+str(squirrel.y)+")")
+                    
+          
+            pygame.display.update()
+        time_count = time_count + 1
+        print("Congrats you have protected your squirrels for " + str(time_count) + " turns")
         turns+=1
  
                 
@@ -315,7 +259,7 @@ def main():
 def squirrelImage(numb):
     hope = True
     if (numb==1):
-        return 'squirrel_template_1.png'
+        return 'whitesquirrel.png'
     if (numb==2):
         return 'squirrel_template_2.png'
     if (numb==3):
